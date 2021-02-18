0ed9b8a53c4b7bc83245e30d3e9e23bc168d973e
Florian Latzel <florian.latzel@gmail.com>
2021-02-18 22:37:22 +0100

    Added --cli to call script on target system too.

--- uberspace_jekyll_deployment.sh
+++ uberspace_jekyll_deployment.sh
@@ -6,24 +6,29 @@
 # See https://netzaffe.de/jekyll-deployment-auf-uberspace-via-bare-repo-und-post-receive-hook/ 
 # for requirements and more detailed description (german)
 
-[ "$1" != "--cli" ] && read oldrev newrev ref ; pushed_branch=${ref#refs/heads/}
+if [[ $# -eq 1 && $1 == "--cli" ]] 
+then
+	pushed_branch="NONE" 
+else
+	read oldrev newrev ref 
+	pushed_branch=${ref#refs/heads/} 
+fi
 
 ## Variables
 
-build_branch='master'                 		# e.g. 'master'
-pushed_branch=${pushed_branch:-$build_branch} 	# default for calling with --cli
-site='netzaffe.de'                    		# e.g. 'example.com'
-site_prefix=''         				# e.g. 'preview.' # Mind the trailing '.' !
+build_branch='master'	# e.g. 'master'
+site='netzaffe.de'	# e.g. 'example.com'
+site_prefix=''         	# e.g. 'preview.' # Mind the trailing '.' !
 
 # Uberspace specific variables
+
 git_repo=${HOME}/repos/${site}.git 
 www=/var/www/virtual/${USER}/${site_prefix}${site}
 
-tmp=$(mktemp -d)
-
 ## Do the magic
  
-[ "$pushed_branch" != "$build_branch" ] && exit
+[[ "$pushed_branch" != "$build_branch" && "$pushed_branch" != "NONE" ]] && exit
+tmp=$(mktemp -d)
 git clone ${git_repo} ${tmp}
 cd ${tmp}
 bundle install --path=~/.gem || bundle install --path=~/.gem --redownload

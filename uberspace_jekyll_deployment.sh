52d9eae184713688d1d27b053e6066f7c985d68d
Florian Latzel <florian.latzel@gmail.com>
2021-03-26 23:09:31 +0100

    Outsourced config variables to build.conf
    - Build script try to source build.conf from bare repository
    - if build.conf does not exist, exit

--- uberspace_jekyll_deployment.sh
+++ uberspace_jekyll_deployment.sh
@@ -5,6 +5,7 @@
 #
 # See https://netzaffe.de/jekyll-deployment-auf-uberspace-via-bare-repo-und-post-receive-hook/ 
 # for requirements and more detailed description (german)
+# set -x
 
 if [[ $# -eq 1 && $1 == "--cli" ]] 
 then
@@ -16,19 +17,9 @@ fi
 
 ## Variables
 
-# Branch that should be build, e.g. 'master'
-build_branch='master'			
-
-# Subdomain, e.g. 'sub.' Optional, mind the trailing '.' !
-subdomain=''
-# Domain, e.g. 'example.com'.
-domain='netzaffe.de'			
-
-# Path to your Jekyll Git-Repository 
-git_repo=${HOME}/repos/${domain}.git	
-
-# Path to document root, destination where the the generated html is served.
-www=/var/www/virtual/${USER}/${subdomain}${domain}
+config=$(mktemp)
+git-show HEAD:build.conf > ${config} || exit
+source ${config}
 
 ## Do the magic
  
@@ -38,5 +29,5 @@ git clone ${git_repo} ${tmp}
 cd ${tmp}
 bundle install || bundle install --redownload
 bundle exec jekyll build --source ${tmp} --destination ${www}
-rm -rf ${tmp}
+rm -rf ${tmp} ${config}
 exit
